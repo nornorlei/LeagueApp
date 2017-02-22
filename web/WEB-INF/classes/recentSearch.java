c7d99d014770ff3ed399ddfc169ba7c2151bfcf5
norman <leinorman129@gmail.com>
2017-02-22 13:02:16 -0500

    d

--- web/WEB-INF/classes/recentSearch.java
+++ web/WEB-INF/classes/recentSearch.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class recentSearch extends HttpServlet {
 
-    
+sssssssssssssssssssssssssssssssssssssssssss
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         String username = (String) request.getSession().getAttribute("username");
